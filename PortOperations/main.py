38873ea7f15cd9715b67731460d74fc2123f182c
lokendra2812 <erlokendra.agrawal@gmail.com>
2024-09-28 14:54:41 +0530

    added heading and informatin why this solution required

--- PortOperations/main.py
+++ PortOperations/main.py
@@ -6,6 +6,17 @@ st.title("Automating Port Operations using Deep Learning")
 st.title("Company Name : Marina Pier Inc.")
 st.write("## Developed by : Mr. Lokendra Kumar Agrawal")
 
-
+# Information providing user : why this solution required and what its doing
+st.write("""
+## The company’s management has set out to build a bias-free/ corruption-free automatic system that reports & avoids faulty situations caused by human error. Examples of human error include miss classifying the correct type of boat. The type of boat that enters the port region is as follows.
+    • Buoy
+    • Cruise_ship
+    • Ferry_boat
+    • Freight_boar
+    • Gondola
+    • Inflatable_boat
+    • Kayak
+    • Paper_boat
+""")
 
 
